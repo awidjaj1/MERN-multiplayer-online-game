e5ced2828202c033418b5849fbfd22214c99accf
Andrew Widjaja <awidjaj1@terpmail.umd.edu>
2024-07-29 13:41:59 -0400

    .

--- server/classes/Entity.js
+++ server/classes/Entity.js
@@ -1,10 +1,11 @@
+import { Climb_N, Climb_S, Idle_E, Idle_N, Idle_NE, Idle_NW, Idle_S, Idle_SE, Idle_SW, Idle_W, Walk_E, Walk_N, Walk_NE, Walk_NW, Walk_S, Walk_SE, Walk_SW, Walk_W } from "./State";
+
 export default class Entity{
     constructor(entity){
         this.name = entity.username; 
         this.level = entity.level; 
         this.x = entity.x; 
         this.y = entity.y;
-        this.state = "idle";
-        this.elevation = 0;
+        this.elevation = entity.elevation;
     }
 }
--- server/classes/Player.js
+++ server/classes/Player.js
@@ -1,6 +1,17 @@
 export default class Player extends Entity{
     constructor(player){
         super(player);
+        this.states = [
+            new Idle_N(), new Idle_NE(), new Idle_E(), new Idle_SE(), new Idle_S(), new Idle_SW(), new Idle_W(), new Idle_NW(),
+            new Walk_N(), new Walk_NE(), new Walk_E(), new Walk_SE(), new Walk_S(), new Walk_SW(), new Walk_W(), new Walk_NW(),
+            new Climb_N(), new Climb_S(),
+        ]
+        this.state = this.states[4];
+        this.speedX = 0;
+        this.speedY = 0;
+        this.fps = 60;
+        this.frameTimer = 0;
+        this.frameInterval = 1000 / this.fps;
 
     }
 }
