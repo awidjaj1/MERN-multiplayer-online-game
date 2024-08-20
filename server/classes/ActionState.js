5b2f1c6ee90ed9f9f4e36d8041e6438871a0def3
Andrew Widjaja <awidjaj1@terpmail.umd.edu>
2024-08-20 15:53:39 -0400

    .

--- server/classes/ActionState.js
+++ server/classes/ActionState.js
@@ -1,25 +1,26 @@
+const BLINK_START = 0;
+const BLINK_END = 7;
+const DASH_START = 8;
+const DASH_END = 15;
+
 export class ActionState{
     static STATES = {
-        IDLE_S: 0,
-        IDLE_N: 1,
-        IDLE_E: 2,
-        IDLE_SE: 3,
-        IDLE_NE: 4,
-        IDLE_W: 5,
-        IDLE_SW: 6,
-        IDLE_NW: 7,
-        WALK_S: 8,
-        WALK_N: 9,
-        WALK_E: 10,
-        WALK_SE: 11,
-        WALK_NE: 12,
-        WALK_W: 13,
-        WALK_SW: 14,
-        WALK_NW: 15,
-        CLIMB_IDLE_N: 16,
-        CLIMB_IDLE_S: 17,
-        CLIMB_N: 18,
-        CLIMB_S: 19,
+        BLINK_S: 0,
+        BLINK_N: 1,
+        BLINK_E: 2,
+        BLINK_SE: 3,
+        BLINK_NE: 4,
+        BLINK_W: 5,
+        BLINK_SW: 6,
+        BLINK_NW: 7,
+        DASH_S: 8,
+        DASH_N: 9,
+        DASH_E: 10,
+        DASH_SE: 11,
+        DASH_NE: 12,
+        DASH_W: 13,
+        DASH_SW: 14,
+        DASH_NW: 15,
     };
     
     static DIRECTIONS = {
@@ -50,4 +51,30 @@ export class ActionState{
     exit(player){
 
     }
+}
+
+class DirectionalActionState extends ActionState{
+    constructor(direction, state, startFrame, endFrame){
+        super(state, startFrame, endFrame);
+        this.direction = direction;
+    }
+
+    handleInput(inputs, player){
+        
+            
+    }
+}
+
+class Blink extends DirectionalActionState{
+    constructor(direction, state){
+        super(direction, state, BLINK_START, BLINK_END);
+    }
+    
+    enter(player){
+
+    }
+}
+
+class Blink_N extends Blink{
+
 }
